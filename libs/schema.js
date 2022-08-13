119491782702029a5a5ed99da746b3c80e08b997
MR-Addict <2750417853@qq.com>
2022-08-13 23:41:20 +0800

    Only allow group be defined!

--- libs/schema.js
+++ libs/schema.js
@@ -1,9 +1,9 @@
 const Joi = require("joi");
 
 const schema = Joi.object().keys({
-  group: Joi.string().min(2).max(10).required(),
   name: Joi.string().min(2).max(10).required(),
   notes: Joi.string().min(4).max(500).required(),
+  group: Joi.string().valid("航模组", "电子组", "编程组", "静模组").required(),
 });
 
 module.exports = schema;
