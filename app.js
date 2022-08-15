fe506aac42916f5b8e88c05304c66a1e3c751302
MR-Addict <2750417853@qq.com>
2022-08-16 00:52:27 +0800

    Add feature:notes review

--- app.js
+++ app.js
@@ -8,11 +8,40 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-const current_year = { current_year: new Date().getFullYear() };
+const home_render = { current_year: new Date().getFullYear() };
+const notes_render = {
+  current_year: new Date().getFullYear(),
+  notes: [{ Error: "Databse not connected!" }],
+};
+
+// Render notes page
+app.get("/notes", (req, res) => {
+  punch_db.query("SELECT * FROM punch", function (err, result, fields) {
+    if (err) {
+      console.error(err);
+      res.render("notes/index", notes_render);
+    } else {
+      notes_render.notes = result;
+      res.render("notes/index", notes_render);
+    }
+  });
+});
+
+app.post("/notes", (req, res) => {
+  punch_db.query(req.body.command, function (err, result, fields) {
+    if (err) {
+      console.error(err);
+      res.render("notes/index", notes_render);
+    } else {
+      notes_render.notes = result;
+      res.render("notes/index", notes_render);
+    }
+  });
+});
 
 // Render home pages
 app.get("/", (req, res) => {
-  res.render("index/index", current_year);
+  res.render("index/index", home_render);
 });
 
 // Post requests
@@ -23,15 +52,15 @@ app.post("/", (req, res) => {
 
   if (validate_result.error) {
     console.error(validate_result.error);
-    res.status(502).render("fail/index", current_year);
+    res.status(502).render("fail/index", home_render);
   } else {
     punch_db.query(punch_sql, punch_record, (err, result) => {
       if (err) {
         console.error(err);
-        res.status(502).render("fail/index", current_year);
+        res.status(502).render("fail/index", home_render);
       } else {
         console.log("New record inserted successfully!");
-        res.render("success/index", current_year);
+        res.render("success/index", home_render);
       }
     });
   }
