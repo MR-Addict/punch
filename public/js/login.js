c9c3a7cb787177abe36a3a93a284c4c5e73667e2
MR-Addict <2750417853@qq.com>
2022-08-23 22:58:59 +0800

    update:replace login validation with custom validation

--- public/js/login.js
+++ public/js/login.js
@@ -2,16 +2,16 @@ import formValidator from "./formValidator.js";
 const fv = new formValidator(".container-body");
 
 // name validation
-fv.register("#name", (value, inputField) => {
+fv.register("#username", (value, inputField) => {
   if (value.length === 0) {
     return {
       pass: false,
-      error: "姓名不能为空哦！",
+      error: "用户名不能为空哦！",
     };
   } else if (value.length > 10) {
     return {
       pass: false,
-      error: "名字太长啦！",
+      error: "用户名太长啦！",
     };
   }
   return {
@@ -19,22 +19,17 @@ fv.register("#name", (value, inputField) => {
   };
 });
 
-// notes validation
-fv.register("#notes", (value, inputField) => {
+// password validation
+fv.register("#password", (value, inputField) => {
   if (value.length === 0) {
     return {
       pass: false,
-      error: "笔记内容不能为空哦！",
+      error: "密码不能为空哦！",
     };
-  } else if (value.length < 4) {
+  } else if (value.length > 20) {
     return {
       pass: false,
-      error: "笔记内容至少4个字哦！",
-    };
-  } else if (value.length > 500) {
-    return {
-      pass: false,
-      error: "你写的笔记太长啦！",
+      error: "密码太长啦！",
     };
   }
   return {
