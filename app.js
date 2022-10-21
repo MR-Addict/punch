77d2d81f1e49d00d6c9604e1a00be486fe0eba86
Cael <2750417853@qq.com>
2022-10-22 00:29:34 +0800

    finish charts API

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ const authorization = (req, res, next) => {
   try {
     const data = jwt.verify(token, "LOGIN_SECRET_KEY");
     req.userId = data.id;
-    req.userRole = data.role;
     return next();
   } catch {
     return res.status(403).json({ status: false, message: "Forbidden!" });
@@ -65,27 +64,29 @@ app.post("/", (req, res) => {
 
 // login and set token
 app.post("/login", (req, res) => {
-  const login_user = req.body;
+  const login_user = JSON.parse(JSON.stringify(req.body));
   const punch_sql = "SELECT * FROM admin";
   const validate_result = punch_schema.login_schema.validate(login_user);
   if (validate_result.error) {
     console.error(validate_result.error);
     return res.status(403).json({ status: false, message: "Forbidden!" });
   } else {
-    punch_db.pool_select.query(punch_sql, async (err, result, fields) => {
+    punch_db.pool_select.query(punch_sql, (err, result, fields) => {
       if (err) {
         console.error(err);
         return res.status(500).json({ status: false, message: "Error!" });
       } else {
         const user = result.find((user) => (user.username = login_user.username));
         if (user == null) {
+          console.log("No such user!");
           return res.status(400).send({ status: false, message: "No such user!" });
         } else {
           try {
-            if (await bcrypt.compare(login_user.password, user.password)) {
-              const token = jwt.sign({ id: user.id }, "LOGIN_SECRET_KEY", { expiresIn: "30min" });
+            if (bcrypt.compare(login_user.password, user.password)) {
+              const token = jwt.sign({ id: user.id }, "LOGIN_SECRET_KEY", { expiresIn: "1h" });
               return res.cookie("accessToken", token).json({ status: true, message: token });
             } else {
+              console.log("Password incorrect!");
               return res.status(502).send({ status: false, message: "Password incorrect!" });
             }
           } catch (err) {
@@ -99,7 +100,7 @@ app.post("/login", (req, res) => {
 });
 
 // logout
-app.get("/logout", authorization, (req, res) => {
+app.get("/logout", (req, res) => {
   return res.clearCookie("accessToken").status(200).json({ status: true, message: "Logout success!" });
 });
 
@@ -109,8 +110,70 @@ app.all("/testcookie", authorization, (req, res) => {
 });
 
 app.post("/table", authorization, (req, res) => {
-  const punch_sql = "SELECT * FROM punch";
+  const punch_sql = "SELECT * FROM punch ORDER BY `date` DESC";
   punch_db.pool_select.query(punch_sql, (err, result, fields) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ status: false, message: "Error!" });
+    } else {
+      res.status(200).send({ status: true, message: JSON.stringify(result) });
+    }
+  });
+});
+
+// Export mysql data
+app.get("/export", authorization, (req, res) => {
+  const punch_sql = "SELECT * FROM punch ORDER BY `date` DESC";
+  punch_db.pool_select.query(punch_sql, (err, result, fields) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ status: false, message: "Error!" });
+    } else {
+      const punch_export = JSON.parse(JSON.stringify(result));
+      const workbook = new excel.Workbook();
+      const worksheet = workbook.addWorksheet("值班笔记");
+      worksheet.columns = Object.keys(punch_export[0]).map((prop) => ({ header: prop, key: prop }));
+      worksheet.addRows(punch_export);
+      Object.keys(punch_export[0]).forEach((prop) => {
+        worksheet.getColumn(prop).width = 15;
+        worksheet.getColumn(prop).font = { size: 13 };
+        worksheet.getColumn(prop).alignment = { vertical: "middle", horizontal: "left", wrapText: true };
+        if (prop === "notes") worksheet.getColumn(prop).width = 100;
+      });
+      worksheet.getRow(1).font = { size: 16, bold: true, color: { argb: "00008B" } };
+      res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+      res.setHeader("Content-Disposition", "attachment; filename=" + encodeURI("值班笔记") + ".xlsx");
+      return workbook.xlsx.write(res).then(() => {
+        res.status(200).end();
+      });
+    }
+  });
+});
+
+app.get("/cards", authorization, (req, res) => {
+  punch_db.pool_select.query(punch_db.analyze_command.cards_sql, (err, result, fields) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ status: false, message: "Error!" });
+    } else {
+      res.status(200).send({ status: true, message: JSON.stringify(result) });
+    }
+  });
+});
+
+app.get("/days", authorization, (req, res) => {
+  punch_db.pool_select.query(punch_db.analyze_command.days_sql, (err, result, fields) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send({ status: false, message: "Error!" });
+    } else {
+      res.status(200).send({ status: true, message: JSON.stringify(result) });
+    }
+  });
+});
+
+app.get("/weeks", authorization, (req, res) => {
+  punch_db.pool_select.query(punch_db.analyze_command.weeks_sql, (err, result, fields) => {
     if (err) {
       console.error(err);
       res.status(500).send({ status: false, message: "Error!" });
--- libs/pool.js
+++ libs/pool.js
@@ -1,11 +1,10 @@
 const mysql = require("mysql");
 const analyze_command = {
-  sum_cmd:
-    "SELECT (SELECT COUNT(*) FROM `punch` WHERE DATE(`date`) = CURRENT_DATE) AS '今日提交',(SELECT COUNT(*) FROM `punch` WHERE WEEK(`date`,1) = WEEK(CURRENT_DATE(),1)) AS '本周提交',(SELECT COUNT(*) FROM `punch`) AS '所有提交'",
-  group_cmd:
-    "SELECT (SELECT COUNT(*) FROM `punch` WHERE `group`='航模组') AS '航模组',(SELECT COUNT(*) FROM `punch` WHERE `group`='编程组') AS '编程组',(SELECT COUNT(*) FROM `punch` WHERE `group`='电子组') AS '电子组',(SELECT COUNT(*) FROM `punch` WHERE `group`='静模组') AS '静模组'",
-  days_cmd:
-    "SELECT `date` AS '日期', COUNT(*) AS '提交次数' FROM `punch` GROUP BY DATE(`date`) ORDER BY `日期` DESC LIMIT 20",
+  cards_sql:
+    "SELECT (SELECT COUNT(*) FROM `punch` WHERE DATE(`date`) = CURRENT_DATE) AS '今日提交',(SELECT COUNT(*) FROM `punch` WHERE WEEK(`date`,1) = WEEK(CURRENT_DATE(),1)) AS '本周提交',(SELECT COUNT(*) FROM `punch` WHERE MONTH(`date`) = MONTH(CURRENT_DATE())) AS '本月提交',(SELECT COUNT(*) FROM `punch`) AS '所有提交'",
+  weeks_sql:
+    "SELECT WEEK(date) AS `周次`,COUNT(*) AS '航模组' FROM `punch` WHERE `group`='航模组' GROUP BY WEEK(date);SELECT WEEK(date) AS `周次`,COUNT(*) AS '编程组' FROM `punch` WHERE `group`='编程组' GROUP BY WEEK(date);SELECT WEEK(date) AS `周次`,COUNT(*) AS '电子组' FROM `punch` WHERE `group`='电子组' GROUP BY WEEK(date);SELECT WEEK(date) AS `周次`,COUNT(*) AS '静模组' FROM `punch` WHERE `group`='静模组' GROUP BY WEEK(date);SELECT COUNT(*) AS '周次' FROM `punch` GROUP BY WEEK(date)",
+  days_sql: "SELECT `date` AS '时间', COUNT(*) AS '次数' FROM `punch` GROUP BY DATE(`date`) ORDER BY `时间`",
 };
 
 const pool_insert = mysql.createPool({
@@ -18,10 +17,11 @@ const pool_insert = mysql.createPool({
 
 const pool_select = mysql.createPool({
   connectionLimit: 4,
-  host: "localhost",
-  user: "punch_select",
-  password: "password",
+  host: "mraddict.top",
+  user: "cael",
+  password: "@Cjw20001212",
   database: "punch",
+  multipleStatements: true,
 });
 
 pool_insert.getConnection((err, connection) => {
