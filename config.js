98e69b10a98b98de412bbd9bd0128a9ac089f131
MR-Addict <2750417853@qq.com>
2022-11-10 00:57:35 +0800

    update week sql

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const sql_cmds = {
     cards_sql:
       "SELECT (SELECT COUNT(*) FROM `punch` WHERE DATE(`date`) = CURRENT_DATE) AS '今日提交',(SELECT COUNT(*) FROM `punch` WHERE WEEK(`date`,1) = WEEK(CURRENT_DATE(),1)) AS '本周提交',(SELECT COUNT(*) FROM `punch` WHERE MONTH(`date`) = MONTH(CURRENT_DATE())) AS '本月提交',(SELECT COUNT(*) FROM `punch`) AS '所有提交'",
     weeks_sql:
-      "SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '航模组' FROM `punch` WHERE `group`='航模组' GROUP BY WEEK(`date`);SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '编程组' FROM `punch` WHERE `group`='编程组' GROUP BY WEEK(`date`);SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '电子组' FROM `punch` WHERE `group`='电子组' GROUP BY WEEK(`date`);SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '静模组' FROM `punch` WHERE `group`='静模组' GROUP BY WEEK(`date`);SELECT COUNT(*) AS '周次' FROM `punch` GROUP BY WEEK(`date`)",
+      "SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '航模组' FROM `punch` WHERE `group`='航模组' GROUP BY `周次`;SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '编程组' FROM `punch` WHERE `group`='编程组' GROUP BY `周次`;SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '电子组' FROM `punch` WHERE `group`='电子组' GROUP BY `周次`;SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '静模组' FROM `punch` WHERE `group`='静模组' GROUP BY `周次`;SELECT WEEK(`date`) AS `周次`,COUNT(*) AS '次数' FROM `punch` GROUP BY `周次`",
     days_sql: "SELECT `date` AS '时间', COUNT(*) AS '次数' FROM `punch` GROUP BY DATE(`date`) ORDER BY `时间`",
   },
   select: {
